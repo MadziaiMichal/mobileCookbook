5cdcc868f45900e31ad71e04356a77cf2d76dc4d
MadziaiMichal <mm.kaniewska.michalak@gmail.com>
2018-09-23 17:50:14 +0200

    Main Activity positioning

--- app/src/main/java/pl/michalak/kaniewska/mm/mobilecookbook/MainActivity.java
+++ app/src/main/java/pl/michalak/kaniewska/mm/mobilecookbook/MainActivity.java
@@ -12,14 +12,47 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonBreakfast;
+    Button buttonDinner;
+    Button buttonSupper;
+    Button buttonDessert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonBreakfast=(Button)findViewById(R.id.button);
+        buttonBreakfast= findViewById(R.id.button);
         buttonBreakfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, MealsList.class);
+                startActivity(i);
+            }
+        });
+
+        buttonDinner= findViewById(R.id.button2);
+        buttonDinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, MealsList.class);
+                startActivity(i);
+            }
+        });
+
+        buttonSupper= findViewById(R.id.button3);
+        buttonSupper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, MealsList.class);
+                startActivity(i);
+            }
+        });
+
+        buttonDessert= findViewById(R.id.button4);
+        buttonDessert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
