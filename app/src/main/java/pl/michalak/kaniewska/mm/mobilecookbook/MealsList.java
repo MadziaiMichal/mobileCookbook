5d8f7205399147f60efe123a89fa1041e36c47a7
Michał <michalak.m15@gmail.com>
2018-09-24 18:27:45 +0200

    dodanie listy posiłków

--- app/src/main/java/pl/michalak/kaniewska/mm/mobilecookbook/MealsList.java
+++ app/src/main/java/pl/michalak/kaniewska/mm/mobilecookbook/MealsList.java
@@ -1,15 +1,23 @@
 package pl.michalak.kaniewska.mm.mobilecookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MealsList extends AppCompatActivity {
 
     // Array of strings...
     ListView simpleList;
-    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand", "China", "australia", "Portugle", "America", "NewZealand"};
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,14 @@ public class MealsList extends AppCompatActivity {
         simpleList.setAdapter(arrayAdapter);
 
 
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MealsList.this, Recipe.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/pl/michalak/kaniewska/mm/mobilecookbook/RecipeFragment.java
@@ -0,0 +1,22 @@
+package pl.michalak.kaniewska.mm.mobilecookbook;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RecipeFragment extends Fragment {
+
+    public RecipeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recipe, container, false);
+    }
+}
